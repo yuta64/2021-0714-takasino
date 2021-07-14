18a9fbba9301893fe62d57e8633f7533c7061f47
yuta <jampingdolfinyuta27@gmail.com>
2021-07-14 09:56:27 +0900

    20210714

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const array = [2, 4, 7, 5, 4, 3, 8];
 const filterArray = array.filter(function (ele, pos) {
-  return array.indexOf(ele)==pos
+  return array.indexOf(ele) == pos
 })
 
 console.log(filterArray);
 
 
 function leapYear(year) {
-  if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     return true;
   }
   return false;
@@ -15,9 +15,10 @@ function leapYear(year) {
 
 let resultYear = 2021;
 if (leapYear(resultYear)) {
-  console.log(resultYear + "はうるう年");
+  console.log(resultYear + "年うるう年");
 } else {
-  console.log(resultYear + "はうるう年でない");
+  console.log(resultYear + "年はうるう年でない");
+
 }
 
 
